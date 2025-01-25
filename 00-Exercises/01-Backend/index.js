38363bb0f2cb9831fd35028d2f0cf77398226e19
Sunanda Roy <roysunanda.update@gmail.com>
2025-01-26 02:18:22 +0530

    backend exercise updated

---
+++ 00-Exercises/01-Backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.json({ msg: `This is Homepage.` });
+});
+
+app.listen(PORT, () => {
+  console.log(`\nServer is listening on port: ${PORT}`);
+});
