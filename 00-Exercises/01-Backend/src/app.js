6bf645caeae0aac8c393d7ee86e6318322681584
Sunanda Roy <roysunanda.update@gmail.com>
2025-01-30 03:05:27 +0530

    backend exercise updated

--- 00-Exercises/01-Backend/src/app.js
+++ 00-Exercises/01-Backend/src/app.js
@@ -1,8 +1,22 @@
 import express from "express";
-// import path from "path";
+import path from "path";
+
+import { router as adminRoutes } from "./routes/admin.js";
+import { router as shopRoutes } from "./routes/shop.js";
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve("public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.sendFile(path.resolve("views", "404.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`\nServer is listening on port: ${PORT}`);
--- 00-Exercises/01-Backend/src/routes/admin.js
+++ 00-Exercises/01-Backend/src/routes/admin.js
@@ -3,13 +3,16 @@ import path from "path";
 
 const router = Router();
 
+let products = [];
+
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.resolve("views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-export { router };
+export { router, products };
