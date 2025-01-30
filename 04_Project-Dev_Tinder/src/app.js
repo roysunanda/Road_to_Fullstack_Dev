db64e93e96764f9a12505b763618f5552c32f88a
Sunanda Roy <roysunanda.update@gmail.com>
2025-01-31 02:33:12 +0530

    dev tinder project updated

--- 04_Project-Dev_Tinder/src/app.js
+++ 04_Project-Dev_Tinder/src/app.js
@@ -4,11 +4,15 @@ const app = express();
 const PORT = 3000;
 
 app.use("/test", (req, res) => {
-  res.send(`this is test page.`);
+  res.send(`<h1>first page.</h1>`);
+});
+
+app.use("/test/2", (req, res) => {
+  res.send(`<h1>this is test page 2.</h1>`);
 });
 
 app.use((req, res) => {
-  res.send(`this is home page.`);
+  res.send(`<h1>home page.</h1>`);
 });
 
 app.listen(PORT, () => {
