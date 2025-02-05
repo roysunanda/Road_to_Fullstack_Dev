deca79905df63af7baf464e9ef8e79d877a1f887
Sunanda Roy <roysunanda.update@gmail.com>
2025-02-05 23:33:41 +0530

    dev tinder updated

--- 04_Project-Dev_Tinder/src/app.js
+++ 04_Project-Dev_Tinder/src/app.js
@@ -20,7 +20,57 @@ app.post("/signup", async (req, res) => {
 app.get("/feed", async (req, res) => {
   try {
     const users = await User.find({});
+    // console.log(users.length);
     res.status(200).send(users);
+  } catch (error) {
+    res.status(400).send(`Something went wrong!`);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ emailId: req.body.email });
+    if (!user) {
+      return res.status(404).send(`user not found`);
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send(`Something went wrong!`);
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  try {
+    // console.log(req.params.id);
+    const id = req.params.id;
+    const user = await User.findById(id);
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send(`Something went wrong!`);
+  }
+});
+
+app.delete("/:id", async (req, res) => {
+  try {
+    // console.log(req.params.id);
+    const id = req.params.id;
+    const user = await User.findByIdAndDelete(id);
+    res.status(200).json({ status: "success", data: user });
+  } catch (error) {
+    res.status(400).send(`Something went wrong!`);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const id = req.body.id;
+    const data = req.body.data;
+    const user = await User.findByIdAndUpdate(
+      id,
+      { firstName: data },
+      { returnDocument: "after" }
+    );
+    res.status(200).json({ status: "success", data: user });
   } catch (error) {
     res.status(400).send(`Something went wrong!`);
   }
