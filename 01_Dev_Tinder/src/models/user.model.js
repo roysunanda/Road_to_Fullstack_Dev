141327a457bd5d2fa87a5555c603b3d545ce3c5b
Sunanda Roy <roysunanda.update@gmail.com>
2025-02-14 23:55:39 +0530

    project updated

--- 01_Dev_Tinder/src/models/user.model.js
+++ 01_Dev_Tinder/src/models/user.model.js
@@ -1,38 +1,41 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: { type: String },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+    },
+    photoUrl: {
+      type: String,
+      default: "https://picsum.photos/200",
+    },
+    about: {
+      type: String,
+      default: "This is default value.",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: { type: String },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-  photoUrl: {
-    type: String,
-    default: "https://picsum.photos/200",
-  },
-  about: {
-    type: String,
-    default: "This is default value.",
-  },
-  skills: {
-    type: [String],
-  },
-});
+  { timestamps: true }
+);
 
 export const userModel = mongoose.model("User", userSchema);
