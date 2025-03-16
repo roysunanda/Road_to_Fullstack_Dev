add362f1e1f6f6238eff3043af5527e746ecbe2d
Sunanda Roy <roysunanda.update@gmail.com>
2025-03-16 11:34:04 +0530

    repo updated

--- _Exercises/Apna_College/01-MongoDB/src/app.js
+++ _Exercises/Apna_College/01-MongoDB/src/app.js
@@ -1,15 +1,38 @@
 import express from "express";
 import morgan from "morgan";
+import path from "path";
 
 // import { User } from "./models/user-model";
+import { Chat } from "./models/chat.model.js";
+import { chatData } from "./data.js";
 
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(express.json());
+// app.set("views", path.join(import.meta.dirname, "..", "views"));
+app.set("views", path.resolve("views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.json({ success: true });
+  res.send("working");
+});
+
+// app.post("/insert", async (req, res) => {
+//   try {
+//     let allData = await Chat.insertMany(chatData);
+//     res.json({ success: true, msg: allData });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+app.get("/chats", async (req, res) => {
+  try {
+    let chats = await Chat.find();
+    res.render("index", { chats });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 export { app };
---
+++ _Exercises/Personal/02-EJS/src/index.js
@@ -0,0 +1,17 @@
+import { app } from "./app.js";
+// import { connectDB } from "./db/connect-db.js";
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    // await connectDB();
+    app.listen(PORT, () => {
+      console.log(`\nServer is listening on PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`\nServer connection failed!! \nError: ${error}`);
+  }
+};
+
+startServer();
