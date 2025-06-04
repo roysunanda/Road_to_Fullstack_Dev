7bf1494aab231d57ae9b0f92a8482095379cbfa1
Sunanda Roy <roysunanda.update@gmail.com>
2025-06-05 01:37:18 +0530

    Travel list app started

--- 00-Exercises/Udemy/Jonas_Schmedtmann/React_Course/04-Travel_List_App/src/App.jsx
+++ 00-Exercises/Udemy/Jonas_Schmedtmann/React_Course/04-Travel_List_App/src/App.jsx
@@ -1,4 +1,56 @@
-function App() {
+import { initialItems } from "./data";
+
+function Logo() {
+  return <h1>🏖️ Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <form className='add-form'>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+      </select>
+      <input type='text' placeholder='Item...' />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : null}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
+
+export function App() {
   return (
     <>
       <div className='app'>
@@ -10,29 +62,3 @@ function App() {
     </>
   );
 }
-
-function Logo() {
-  return <h1>🏖️ Far Away 💼</h1>;
-}
-
-function Form() {
-  return (
-    <div className='add-form'>
-      <h3>What do you need for your 😍 trip?</h3>
-    </div>
-  );
-}
-
-function PackingList() {
-  return <div className='list'>LIST</div>;
-}
-
-function Stats() {
-  return (
-    <footer className='stats'>
-      <em>You have X items on your list, and you already packed X (X%)</em>
-    </footer>
-  );
-}
-
-export { App };
