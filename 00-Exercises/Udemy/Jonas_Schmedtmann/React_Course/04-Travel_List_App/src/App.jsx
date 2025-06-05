91dcb0122ddc5c69e4471042e5f20c815ddfb328
Sunanda Roy <roysunanda.update@gmail.com>
2025-06-06 00:54:57 +0530

    Travel list app updated

--- 00-Exercises/Udemy/Jonas_Schmedtmann/React_Course/04-Travel_List_App/src/App.jsx
+++ 00-Exercises/Udemy/Jonas_Schmedtmann/React_Course/04-Travel_List_App/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { initialItems } from "./data";
 
 function Logo() {
@@ -5,15 +6,35 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
   return (
-    <form className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
-        <option value={1}>1</option>
-        <option value={2}>2</option>
-        <option value={3}>3</option>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
-      <input type='text' placeholder='Item...' />
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
@@ -24,7 +45,7 @@ function PackingList() {
     <div className='list'>
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
