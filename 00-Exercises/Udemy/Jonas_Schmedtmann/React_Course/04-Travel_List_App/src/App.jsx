9a5264671b41ed6313b39bced72c0cff202dc546
Sunanda Roy <roysunanda.update@gmail.com>
2025-06-09 23:32:03 +0530

    11:31pm 09-jun-25

--- 00-Exercises/Udemy/Jonas_Schmedtmann/React_Course/04-Travel_List_App/src/App.jsx
+++ 00-Exercises/Udemy/Jonas_Schmedtmann/React_Course/04-Travel_List_App/src/App.jsx
@@ -5,14 +5,17 @@ function Logo() {
   return <h1>🏖️ Far Away 💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -40,25 +43,25 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className='list'>
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {items.map((item) => (
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : null}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
@@ -72,12 +75,20 @@ function Stats() {
 }
 
 export function App() {
+  const [items, setItems] = useState(initialItems);
+  function handleItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
   return (
     <>
       <div className='app'>
         <Logo />
-        <Form />
-        <PackingList />
+        <Form onAddItems={handleItems} />
+        <PackingList items={items} onDeleteItem={handleDeleteItem} />
         <Stats />
       </div>
     </>
