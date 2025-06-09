5f07f5c7463bad9c63c39d72ed07e3a5f3a22c2f
Sunanda Roy <roysunanda.update@gmail.com>
2025-06-10 02:09:59 +0530

    2:09am 10-jun-25

--- 00-Exercises/Udemy/Jonas_Schmedtmann/React_Course/04-Travel_List_App/src/App.jsx
+++ 00-Exercises/Udemy/Jonas_Schmedtmann/React_Course/04-Travel_List_App/src/App.jsx
@@ -43,21 +43,31 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItems }) {
   return (
     <div className='list'>
       <ul>
         {items.map((item) => (
-          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            key={item.id}
+            onToggleItems={onToggleItems}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItems }) {
   return (
     <li>
+      <input
+        type='checkbox'
+        value={item.packed}
+        onChange={() => onToggleItems(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : null}>
         {item.quantity} {item.description}
       </span>
@@ -66,10 +76,26 @@ function Item({ item, onDeleteItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className='stats'>
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className='stats'>
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? `You got everything! Ready to go ✈️`
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
@@ -83,13 +109,25 @@ export function App() {
   function handleDeleteItem(id) {
     setItems((items) => items.filter((item) => item.id !== id));
   }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <>
       <div className='app'>
         <Logo />
         <Form onAddItems={handleItems} />
-        <PackingList items={items} onDeleteItem={handleDeleteItem} />
-        <Stats />
+        <PackingList
+          items={items}
+          onDeleteItem={handleDeleteItem}
+          onToggleItems={handleToggleItem}
+        />
+        <Stats items={items} />
       </div>
     </>
   );
