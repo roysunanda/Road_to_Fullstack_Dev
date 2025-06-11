bdce093343c31cb3b4cd360ad36e3a42d4215498
Sunanda Roy <roysunanda.update@gmail.com>
2025-06-11 19:15:10 +0530

    repo updated

--- 00-Exercises/Udemy/Jonas_Schmedtmann/React_Course/04-Travel_List_App/src/App.jsx
+++ 00-Exercises/Udemy/Jonas_Schmedtmann/React_Course/04-Travel_List_App/src/App.jsx
@@ -44,10 +44,25 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItems }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+  if (sortBy === "description") {
+    sortedItems = [...items].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+  }
+  if (sortBy === "packed") {
+    sortedItems = [...items].sort((a, b) =>
+      Number(a.packed - Number(b.packed))
+    );
+  }
   return (
     <div className='list'>
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -56,6 +71,13 @@ function PackingList({ items, onDeleteItem, onToggleItems }) {
           />
         ))}
       </ul>
+      <div className='actions'>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value='input'>Sort by input order</option>
+          <option value='description'>Sort by description</option>
+          <option value='packed'>Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
